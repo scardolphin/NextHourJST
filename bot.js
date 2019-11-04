58b5f6fe61cbf6bccbce49d896d9b8c73dac8636
scardolphin <57325669+scardolphin@users.noreply.github.com>
2019-11-04 20:48:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,9 +18,9 @@ client.on('ready', () => {
       command = message.content.slice (prefix.length).split (" ")[0],
       sec = 3600;
   switch (command) {
-    case "startloop":
+    case "start time":
       if (message.channel.timeout) return message.channel.send ('loop is already started, use []stoploop to stop the loop.');
-      else { message.channel.send('Loop started, will inform you every hour now starting in '+ (Math.round(leftToFiftyFive()/60000))+ ' minutes.Use []stoploop to stop the loop.');
+      else { message.channel.send('will inform you of the time every hour now starting in '+ (Math.round(leftToFiftyFive()/60000))+ ' minutes.Use []stoploop to stop the loop.');
             message.channel.timeout=setTimeout(function(){ // in leftToFiftyFive() milliseconds run this:
             message.channel.send('Current Time in Tokyo: '+new Date().toLocaleTimeString("jp-JP",{timeZone:"Asia/Tokyo"}));
        message.channel.loop = setInterval (() => message.channel.send ('Current Time in Tokyo '+ new Date().toLocaleTimeString("jp-JP",{timeZone:"Asia/Tokyo"})), sec * 1000)
@@ -34,10 +34,10 @@ function leftToFiftyFive(){
 }
            }
       break;
-    case "stoploop":
-      if (!message.channel.timeout) return message.channel.send ('no loop to stop lol');
+    case "stop time":
+      if (!message.channel.timeout) return message.channel.send ('no time to stop lol');
       else {
-       message.channel.send('Loop stopping..');
+       message.channel.send('Time stopped');
         clearInterval (message.channel.loop);
         clearTimeout(message.channel.timeout);
         message.channel.loop = false;
