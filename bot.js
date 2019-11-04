3dba9c840663e1b5135679f0170660680624b87a
scardolphin <57325669+scardolphin@users.noreply.github.com>
2019-11-04 15:37:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,18 +10,51 @@ client.on('ready', () => {
 
 });
 
- 
 
+    
+ client.on ('message', async message => {
+  if(message.content.startsWith('!')){
+  var prefix = "!",
+      command = message.content.slice (prefix.length).split (" ")[0],
+      sec = 10;
+  switch (command) {
+    case "startloop":
+      if (message.channel.timeout) return message.channel.send ('loop is already started, use []stoploop to stop the loop.');
+      else { message.channel.send('Loop started, will inform you every hour now starting in '+ (Math.round(leftToFiftyFive()/60000))+ ' minutes.Use []stoploop to stop the loop.');
+            message.channel.timeout=setTimeout(function(){ // in leftToFiftyFive() milliseconds run this:
+            message.channel.send('Current Time in Tokyo: '+new Date().toLocaleTimeString("jp-JP",{timeZone:"Asia/Tokyo"}));
+       message.channel.loop = setInterval (() => message.channel.send ('Current Time in Tokyo '+ new Date().toLocaleTimeString("jp-JP",{timeZone:"Asia/Tokyo"})), sec * 1000)
+    }, leftToFiftyFive())
+            
+function leftToFiftyFive(){
+    var d = new Date();
+    var d2= -d + d.setMinutes(55,0,0);
+    if(d2 <=0) d2= d2+ 3.6e+6;
+    return d2;    
+}
+           }
+      break;
+    case "stoploop":
+      if (!message.channel.timeout) return message.channel.send ('no loop to stop lol');
+      else {
+       message.channel.send('Loop stopping..');
+        clearInterval (message.channel.loop);
+        clearTimeout(message.channel.timeout);
+        message.channel.loop = false;
+       message.channel.timeout= false;
+      }
+      break;
+  }
+ }
+});
 client.on('message', message => {
 
-    if (message.content === 'ping') {
+    if (message.content === 'curTime') {
 
-       message.reply('pong');
+       message.reply('Current time in Tokyo is '+ new Date().toLocaleTimeString("jp-JP",{timeZone:"Asia/Tokyo"}));
 
        }
-
-});
-
+ });
  
 
 // THIS  MUST  BE  THIS  WAY
